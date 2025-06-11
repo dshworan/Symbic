585e5ff541fa4ef6df685604dac7bc318b79e26e
Dave Shworan <dave@shworan.com>
2025-06-10 23:39:48 -0700

    tutorial wording and spaces

--- src/data/tutorials/pack1Tutorials.ts
+++ src/data/tutorials/pack1Tutorials.ts
@@ -1,16 +1,16 @@
 export const pack1Tutorials = {
   level1: [
-    "Rule 1: There are always  an equal number of each shape in a row or column.",
-    "Rule 2: You are not allowed to have 3 in a row horizontally or vertically.",
-    "Rule 3: No two rows can be exactly the same.",
-    "Rule 4: Also, no two columns can be exactly the same.",
-    "You shouldn't have to guess. Use logic to solve the puzzle."
+    "Rule 1:  There are always an equal number of each shape in a row or column.",
+    "Rule 2:  You are not allowed to have 3 in a row horizontally or vertically.",
+    "Rule 3:  No two rows can be exactly the same.",
+    "Rule 4:  Also, no two columns can be exactly the same.",
+    "You shouldn't have to guess.  Use logic to solve the puzzle."
   ],
   level2: [
     "Use hints if you get stuck, you can always get more hints if needed.",
     "You can also restart the puzzle if you think you've made mistakes.",
     "Puzzles will get harder as you progress.",
     "And there are many Puzzle Packs of varying difficulty levels.",
-    "Have fun! Master the grid!"
+    "Have fun!  Master the grid!"
   ] 
 }; 
